2effd3a7b06d4203b946d7da266cd5d76c455e9c
Corey Gray <csgray@alaska.edu>
2018-04-25 19:47:29 -0800

    Initial commit. Add mapalloc.h and mapalloc.cpp

---
+++ mapalloc.cpp
@@ -0,0 +1,20 @@
+// mapalloc.cpp
+// Corey S. Gray
+// 25 April 2018
+// Source for Assignment 4 - Exercise A: Simple Memory Manager
+
+#include "mapalloc.h"
+
+// mapAlloc
+// Takes a size_t indicating the number of bytes to be allocated.
+// Returns a pointer to the allocated block or nullptr if unsuccessful.
+void * mapAlloc(size_t length) {
+
+}
+
+// mapFree
+// Takes a pointer to a block previous allocated with mapAlloc.
+// Deallocates the given block and returns nothing.
+void mapFree(void *) {
+
+}
---
+++ mapalloc.h
@@ -0,0 +1,12 @@
+// mapalloc.h
+// Corey S. Gray
+// 25 April 2018
+// Header for Assignment 4 - Exercise A: Simple Memory Manager
+
+#ifndef FILE_MAPALLOC_H_INCLUDED
+#define FILE_MAPALLOC_H_INCLUDED
+
+void * mapAlloc(size_t);
+void mapFree(void *);
+
+#endif // #ifndef FILE_MAPALLOC_H_INCLUDED
